98007ca7052958fc8833547f99c4b4d68be6bb43
JP Vergara <JP@JPs-MacBook-Pro.local>
2016-12-02 13:36:57 -0800

    Finished Grunt Tasks, optimized images, deleted node_modules and reinstalls grunt files, edited forelative path for images

--- gruntfile.js
+++ gruntfile.js
@@ -1,42 +1,49 @@
-grunt.initConfig({
-  responsive_images: {
-    dev: {
-      options: {},
-      sizes: [{
-        name: 'small',
-        width: 350,
-        height: 150
-      },{
-        name: 'medium',
-        width: 640
-      },{
-        name: "large",
-        width: 1140,
-        separator: "-"
-        suffix: "_x2",
-        quality: 0.6
-      }]
-      files: [{
-        expand: true,
-        src: ['app/img/**/*.{jpg,gif,png}'],
-        cwd: 'src/',
-        dest: 'dist/'
-      }]
-    }
-}
-copy: {
-  dev: {
-    files: [{
-      expand: true,
-      src: ['**/*', '!app/img/**/*.*'],
-      cwd: 'src/',
-      dest: 'dist/'
-    }]
-  }
-}
-});
+module.exports = function(grunt) {
 
-grunt.loadNpmTasks('grunt-contrib-copy');
-grunt.loadNpmTasks('grunt-responsive-image');
 
-grunt.registerTask('default', ['copy','responsive_images']);
+    grunt.initConfig({
+        responsive_images: {
+          dev: {
+            options: {
+              engine: 'im',
+              sizes: [{
+                name: 'small',
+                width: 350,
+                height: 150
+              }, {
+                name: 'medium',
+                width: 640
+              }, {
+                name: "large",
+                width: 1140,
+                separator: "-",
+                suffix: "_x2",
+                quality: 0.6
+              }]
+            },
+            files: [{
+              expand: true,
+              src: ['*.{jpg,gif,png}'],
+              cwd: 'images_src/',
+              dest: 'images/'
+            }]
+          }
+        },
+        copy: {
+          dev: {
+            files: [{
+              expand: true,
+              src: ['**/*', '!app/img/**/*.*'],
+              cwd: 'src/',
+              dest: 'dist/'
+            }]
+          }
+        }
+      });
+
+      grunt.loadNpmTasks('grunt-contrib-copy');
+      grunt.loadNpmTasks('grunt-responsive-images');
+
+      grunt.registerTask('default',['copy', 'responsive_images']);
+
+    };
